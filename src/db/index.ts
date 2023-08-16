f42e7ca1dbb25aac0300afb95546ff2c68a9cc7c
Tomas Flores <tomy24ssf@gmail.com>
2023-08-16 17:23:53 -0300

    create models and associations

--- src/db/index.ts
+++ src/db/index.ts
@@ -15,8 +15,8 @@ import {
   Service,
   User_type,
 } from '../models';
-
 dotenv.config();
+
 const { DB_PASSWORD, DB_NAME, DB_HOST, DB_USER } = process.env;
 const DB_PORT: number = 5432;
 
@@ -46,14 +46,67 @@ const sequelize = new Sequelize({
   native: false,
 });
 
-// associations
-Provider.belongsToMany(Service, { through: 'provider_service', timestamps: false });
-Service.belongsToMany(Provider, { through: 'provider_service', timestamps: false });
-
+// * associations
+//user
 Country.hasMany(User);
 User.belongsTo(Country);
 
 User_type.hasMany(User);
 User.belongsTo(User_type);
+
+//rating
+User.hasMany(Rating);
+Rating.belongsTo(User);
+
+Provider.hasMany(Rating);
+Rating.belongsTo(Provider);
+
+//company
+User.hasMany(Company);
+Company.belongsTo(User);
+
+//collaborator
+User.hasMany(Collaborator);
+Collaborator.belongsTo(User);
+
+Gender.hasMany(Collaborator);
+Collaborator.belongsTo(Gender);
+
+Company.hasMany(Collaborator);
+Collaborator.belongsTo(Company);
+
+//provider
+User.hasMany(Provider);
+Provider.belongsTo(User);
+
+Gender.hasMany(Provider);
+Provider.belongsTo(Gender);
+
+//purchase_membership
+Company.hasMany(Purchase_membership);
+Purchase_membership.belongsTo(Company);
+
+Membership.hasMany(Purchase_membership);
+Purchase_membership.belongsTo(Membership);
+
+//membership_relationship
+Purchase_membership.hasMany(Membership_relationship);
+Membership_relationship.belongsTo(Purchase_membership);
+
+Status.hasMany(Membership_relationship);
+Membership_relationship.belongsTo(Status);
+
+Service.hasMany(Membership_relationship);
+Membership_relationship.belongsTo(Service);
+
+Collaborator.hasMany(Membership_relationship);
+Membership_relationship.belongsTo(Collaborator);
+
+Provider.hasMany(Membership_relationship);
+Membership_relationship.belongsTo(Provider);
+
+//provider_service
+Provider.belongsToMany(Service, { through: 'provider_service', timestamps: false });
+Service.belongsToMany(Provider, { through: 'provider_service', timestamps: false });
 
 export default sequelize;
--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,8 @@
 import express from 'express';
 import db from './db/index';
+
 const app = express();
 const PORT = 3000;
-
 db.sync({ force: true }).then(() => {
-  app.listen(PORT, () => {
-    console.log(`%s listening at ${PORT}`);
-  });
+  app.listen(PORT, () => console.log(`%s listening at ${PORT}`));
 });
