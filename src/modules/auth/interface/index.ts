f6db9b4c3819e4bee83f4bb62f7b64bd3dbf9cd7
Tomas Flores <tomy24ssf@gmail.com>
2023-08-24 18:26:24 -0300

    refactory: return token

--- src/modules/auth/interface/index.ts
+++ src/modules/auth/interface/index.ts
@@ -1,11 +1,6 @@
-import { Collaborator, Company, Provider } from '../../../models';
+// import { Collaborator, Company, Provider } from '../../../models';
 import { CreateUserDTO } from '../../user/dto/user';
 import { AuthCollaboratorDTO, AuthCompanyDTO, AuthProviderDTO } from '../dto/auth';
-
-// 1 | Empresa
-// 2 | Colaborador
-// 3 | Profesional
-// 4 | Admin
 
 export enum TypesAuth {
   COMPANY = 'Empresa',
@@ -14,12 +9,11 @@ export enum TypesAuth {
 }
 
 type Type = TypesAuth.COLLABORATOR | TypesAuth.COMPANY | TypesAuth.PROVIDER;
-type ModelsAuth = Company | Provider | Collaborator | undefined;
 
 type AuthInDBF = (
   profileData: AuthCompanyDTO | AuthProviderDTO | AuthCollaboratorDTO,
   userData: CreateUserDTO,
   type: Type
-) => Promise<ModelsAuth>;
+) => Promise<{ email: string; UserId: string } | undefined>;
 
 export { AuthInDBF };
